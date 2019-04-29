df02d21e1c44740e11c2a8dec42ca83a11be8d46
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2019-04-29 09:34:32 +0700

    WIP: Landing

---
+++ app/imports/ui/components/UserCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+const UserCard = ({ name, username, avatarUrl, bio, power }) => (
+    <Card fluid className='user-card'>
+        <Card.Content>
+            <Image floated='left' avatar size='huge' src={avatarUrl} />
+            <Card.Header>{name}</Card.Header>
+            <Card.Meta><a href={`https://twitter.com/${username}`}>@{username}</a></Card.Meta>
+            <Card.Description>{bio}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <Icon name='bolt' color='yellow' />
+            {power}
+        </Card.Content>
+    </Card>
+);
+
+UserCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    avatarUrl: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    power: PropTypes.number.isRequired,
+};
+
+export default UserCard;
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import GitHubForkRibbon from 'react-github-fork-ribbon';
+import { Icon, Container, Header, Image, Divider, List, Grid, Segment, Button, Card } from 'semantic-ui-react';
 import ForkRibbon from '../components/ForkRibbon';
-import { Icon, Container, Header, Image, Divider, List, Grid, Segment, Button } from 'semantic-ui-react';
+import UserCard from '../components/UserCard';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -56,44 +56,102 @@ class Landing extends React.Component {
                               </a> too see his decision.
                           </p>
                       </Grid.Column>
-                      <Grid.Column width={6} floated='right' verticalAlign='middle'>
-                          <Image src='/shield.png' size='medium' floated='right' />
+                      <Grid.Column width={6} floated='right' verticalAlign='middle' textAlign='justified'>
+                          <Image src='/shield.png' size='medium' centered />
                       </Grid.Column>
                   </Grid.Row>
                   <Grid.Row>
                       <Grid.Column width={16}>
-                          <Header as='h3'>How it works?</Header>
-                          <List size='huge' relaxed>
+                          <Header as='h3'>How does it work?</Header>
+                          <List size='huge' relaxed className='alice-list'>
                               <List.Item>
-                                  <List.Icon name='bitcoin' />
+                                  <List.Icon name='bitcoin' color='yellow' />
                                   <List.Content>Alice is new to crypto.</List.Content>
                               </List.Item>
                               <List.Item>
-                                  <List.Icon name='question' />
-                                  <List.Content>Alice wants to know: <a href='#'>Is Craig Wright fraud or not?</a></List.Content>
+                                  <List.Icon name='question' color='olive' />
+                                  <List.Content>
+                                      Alice wants to know:
+                                      &nbsp;<a href='#'>Is Craig Wright fraud or not?</a>
+                                  </List.Content>
                               </List.Item>
                               <List.Item>
-                                  <List.Icon name='check' />
+                                  <List.Icon name='check' color='green' />
                                   <List.Content>
                                       Alice checks the votes of her favourite Twitter personalities:
-                                      &nbsp;<a href='#'>@notsofast</a>,
-                                      &nbsp;<a href='#'>@anambroid</a>
+                                      &nbsp;<a
+                                        target='_blank'
+                                        rel='noopener noreferrer'
+                                        href='https://twitter.com/notsofast'>@notsofast</a>,
+                                      &nbsp;<a
+                                        target='_blank'
+                                        rel='noopener noreferrer'
+                                        href='https://twitter.com/anambroid'>@anambroid</a>
                                       &nbsp;& others.
                                   </List.Content>
                               </List.Item>
                               <List.Item>
-                                  <List.Icon name='twitter square' />
+                                  <List.Icon name='twitter square' color='blue' />
                                   <List.Content>Alice <b>follows more people</b> to see their votes.</List.Content>
                               </List.Item>
                               <List.Item>
-                                  <List.Icon name='lab' />
+                                  <List.Icon name='lab' color='purple' />
                                   <List.Content>Alice makes up her mind.</List.Content>
                               </List.Item>
                               <List.Item>
-                                  <List.Icon name='thumbs up' />
-                                  <List.Content>Alice is happy</List.Content>
+                                  <List.Icon name='thumbs up' color='green' />
+                                  <List.Content>Alice is happy.</List.Content>
                               </List.Item>
                           </List>
+                          <div className='sign-in-row'>
+                              <Button primary size='huge' onClick={this.onSignUp}>
+                                  Try it with
+                                  <Icon name='twitter' className='right' />
+                              </Button>
+                          </div>
+                      </Grid.Column>
+                  </Grid.Row>
+                  <Grid.Row>
+                      <Grid.Column width={16}>
+                          <Header as='h3'>Who has already signed up?</Header>
+                          <Grid stackable columns={3} className='influencers-list'>
+                              <Grid.Column>
+                                  <UserCard
+                                      name='John McAfee'
+                                      username='officialmcafee'
+                                      bio='The power we are seeking to unleash with the 2020 campaign is not the power of John McAfee. It is the power of the individual.'
+                                      avatarUrl='https://pbs.twimg.com/profile_images/1068211396712763392/7FxhjlR3_400x400.jpg'
+                                      power={14453232}
+                                  />
+                              </Grid.Column>
+                              <Grid.Column>
+                                  <UserCard
+                                      name='James Bang'
+                                      username='PRHacks'
+                                      bio='Using Blockchain to solve real world problems. Cryptocurrency investor & market maker for exchanges and tokens. CEO of Zeo Fund & COO of ICOiN Studios.'
+                                      avatarUrl='https://pbs.twimg.com/profile_images/1032108604378112001/duH5lAY7_400x400.jpg'
+                                      power={45600787}
+                                  />
+                              </Grid.Column>
+                              <Grid.Column>
+                                  <UserCard
+                                      name='Tyler Jenks'
+                                      username='LucidInvestment'
+                                      bio='President of Lucid Investment with over 40 years experience in financial markets. Devoted to analyzing Bitcoin and Cryptos. Host of Hyperwave Web Series.'
+                                      avatarUrl='https://pbs.twimg.com/profile_images/1003982000796110849/Io557AVG_400x400.jpg'
+                                      power={7651132}
+                                  />
+                              </Grid.Column>
+                              <Grid.Column>
+                                  <UserCard
+                                      name='CryptoPanic HQ'
+                                      username='CryptoPanicHQ'
+                                      bio='This is official http://CryptoPanic.com  announcements channel. Follow @CryptoPanicCom for trending news!'
+                                      avatarUrl='https://pbs.twimg.com/profile_images/1033478926490914818/t02Q6ozB_400x400.jpg'
+                                      power={876552}
+                                  />
+                              </Grid.Column>
+                          </Grid>
                       </Grid.Column>
                   </Grid.Row>
               </Grid>
